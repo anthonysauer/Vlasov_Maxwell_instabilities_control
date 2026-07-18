eb2d51ae77f86c4b5f696f4ca2c82bd07cded175
anthonysauer <asauer3@wisc.edu>
2026-07-18 13:11:56 -0500

    add tanh activation to nn output to keep external fields within a reasonable range

--- vm_control/simple_nn.py
+++ vm_control/simple_nn.py
@@ -1,10 +1,20 @@
 from flax import nnx
+import jax.numpy as jnp
 
 
 class SimpleNN(nnx.Module):
 
-    def __init__(self, n_features: int, n_targets: int, n_hidden: int = 100, *, rngs: nnx.Rngs):
+    def __init__(
+            self,
+            n_features: int,
+            n_targets: int,
+            n_hidden: int = 100,
+            max_amplitude: float = 0.1,
+            *,
+            rngs: nnx.Rngs
+    ):
         self.n_features = n_features
+        self.max_amplitude = max_amplitude
         self.layer1 = nnx.Linear(n_features, n_hidden, rngs=rngs)
         self.layer2 = nnx.Linear(n_hidden, n_hidden, rngs=rngs)
         self.layer3 = nnx.Linear(n_hidden, n_targets, kernel_init=nnx.initializers.zeros, rngs=rngs)
@@ -14,4 +24,7 @@ class SimpleNN(nnx.Module):
         x = nnx.selu(self.layer1(x))
         x = nnx.selu(self.layer2(x))
         x = self.layer3(x)
+
+        # tanh forces the values to be strictly between -1.0 and 1.0
+        x = jnp.tanh(x) * self.max_amplitude
         return x
